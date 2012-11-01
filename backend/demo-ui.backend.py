0f3fa72004e26a69d83d89af458d61afccdd7340
RafiKueng <rafi.kueng@gmx.ch>
2012-11-01 01:25:56 +0100

    create config file

--- backend/demo-ui.backend.py
+++ backend/demo-ui.backend.py
@@ -2,6 +2,7 @@
  
 import sys
 import json
+import subprocess
 
 from twisted.internet import reactor
 from twisted.python import log
@@ -12,6 +13,100 @@ from autobahn.websocket import WebSocketServerFactory, \
                                WebSocketServerProtocol, \
                                listenWS
  
+run_id=0
+ 
+
+class GlassSettings(object):
+  def __init__(self, lensid, runid):
+    self.lens_id = lensid #expected to be a string
+    self.run_id = repr(runid)
+    self.points = []
+    
+  
+  #add them in the right order! fifo
+  def addSource(self, point):
+    self.points.append(point)
+  
+  def setRedshiftLens(self, z):
+    self.zlens = repr(z)
+    
+  def setRedshiftSource(self, z):
+    self.zSrc = repr(z)
+  
+  
+  def genOutput(self):
+    s = self
+    out = """
+glass_basis('glass.basis.pixels', solver='rwalk')
+import pylab as pl
+meta(author='Jonathan Coles', notes='Just testing')
+"""
+    out += "setup_log('" + s.lens_id + "_" + s.run_id + ".log')\n"
+    
+    out += """
+samplex_random_seed(0)
+samplex_acceptance(rate=0.25, tol=0.15)
+
+exclude_all_priors()
+include_prior(
+    'lens_eq', 
+    'time_delay', 
+    'profile_steepness', 
+    'J3gradient', 
+    'magnification',
+    'hubble_constant',
+    'PLsmoothness3',
+    'shared_h',
+    'external_shear'
+)
+
+hubble_time(13.7)
+"""
+
+    out += "globject('" + s.lens_id + "')\n"
+    out += "zlens(" + s.zlens + ")\n"
+    out += """
+pixrad(3)
+steepness(0,None)
+smooth(2,include_central_pixel=False)
+local_gradient(45)
+shear(0.01)
+
+"""
+    
+    for i in range(len(s.points)):
+      out += chr(65+i) + " = " + s.points[i].toStringA() + "\n"
+    
+    out += "\nsource(" + s.zSrc + ", \n"
+    
+    for i in range(len(s.points)):
+      out += chr(65+i) + ", " + s.points[i].toStringB() + (",\n" if i < len(s.points)-1 else ")\n\n")
+      
+    out += "model(1000)\n"
+    
+    s.statefile = s.lens_id + "_" + s.run_id + ".state"
+    out += "savestate('" + s.statefile + "')"
+    
+    f = open('tmp/' + s.lens_id + "_" + s.run_id + ".gls", 'w')
+    f.write(out)
+    f.close()
+
+    
+  
+
+class Point(object):
+  def __init__(self, x,y,type='None',delay='None'):
+    self.x = x
+    self.y = y
+    self.type = type
+    self.delay = '' if delay==None else delay
+  
+  def toStringA(self):
+    return repr(self.x)+','+repr(self.y)
+    
+  def toStringB(self):
+    return "'" + self.type + "' " + ('' if self.delay=='' else ', '+self.delay)
+  
  
 class EchoServerProtocol(WebSocketServerProtocol):
 
@@ -50,8 +145,37 @@ class EchoServerProtocol(WebSocketServerProtocol):
     elif (id=="pnts"):
       print "got pnts: " + data
       
+      #read in the points
+      data = [_.split(':') for _ in data.split('|')]
+      
+      global run_id
+      run_id += 1 
+      gs = GlassSettings(data[0][0],run_id)
+      
+      for i in range(1,len(data)-1):
+        if len(data[i])==4:
+          delay = data[i][3]
+        elif i==0:
+          delay = None
+        else:
+          delay = ''
+        
+        print "got data: ", i, data[i][0], data[i][1], data[i][2], delay
+        pnt = Point(data[i][0], data[i][1], data[i][2], delay)
+        gs.addSource(pnt)
+        
+      #the last line are the other parameters
+      gs.setRedshiftLens(data[-1][0])
+      gs.setRedshiftSource(data[-1][1])      
+      
+      gs.genOutput() 
+        
       #call glass, generate img
-      sleep(5)
+      print "call glass"
+      subprocess.call(['../glass/run_glass', 'tmp/'+gs.statefile])
+      print "return from glass"
+      
+      #sleep(5)
       url = "hubble-udf.jpg"
       self.sendMessage("cont" + url)
       
@@ -59,13 +183,27 @@ class EchoServerProtocol(WebSocketServerProtocol):
       print "PROTOCOLL ERROR - dump:"
       print msg
 
+  def prepareGlassInputFile():
+    pass
   
 if __name__ == '__main__':
- 
-   log.startLogging(sys.stdout)
- 
-   factory = WebSocketServerFactory("ws://localhost:8080", debug = False)
-   factory.protocol = EchoServerProtocol
-   listenWS(factory)
- 
-   reactor.run()
+
+  # print "testing"
+  # gs = GlassSettings(1,1)
+  # pnt = Point(4.3,5.4,'min',None)
+  
+  # gs.addSource(pnt)
+  # gs.addSource(pnt)
+  # gs.addSource(pnt)
+  # gs.setRedshiftLens(0.5)
+  # gs.setRedshiftSource(1)
+  # gs.genOutput()  
+    
+
+  log.startLogging(sys.stdout)
+
+  factory = WebSocketServerFactory("ws://localhost:8080", debug = False)
+  factory.protocol = EchoServerProtocol
+  listenWS(factory)
+
+  reactor.run()
