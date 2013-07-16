b334f5fcd6dca89d4bee27adeb1e968ba9778b03
RafiKueng <rafi.kueng@gmx.ch>
2013-07-16 19:19:34 +0200

    typo

--- static_html/body.php
+++ static_html/body.php
@@ -443,7 +443,7 @@
 
 <div id="wait_for_results_dialog" class="dialog" title="Currently Crunching Numbers">
   <p>Runnung since: <span id="wfrd_running">0</span>s</p>
-  <p>Estimanted duration: <span id="wfrd_est">0</span>s</p>
+  <p>Estimated duration: (very roughly)<span id="wfrd_est">0</span>s</p>
 </div>
 
 <div id="load_progress_dialog" class="dialog" title="Loading images">
