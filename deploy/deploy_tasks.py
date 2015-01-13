907c68ca02f133f4996582f96b83996da548d307
Rafael Kueng <rafi.kueng@gmx.ch>
2015-01-13 16:46:33 +0100

    tmp cleanup

--- deploy/deploy_tasks.py
+++ deploy/deploy_tasks.py
@@ -187,224 +187,186 @@ def deploy_worker():
         
         
     # create startup scripts
-    
-        
-@task()
-def deploy_worker_bkup():
-    
-    _check_if_local_branch_is_master()
-    _generate_version_information()
-    _setup_dirs_and_copy_files_worker()
-
-    with cd(_E.INSTALLPATH):
-
-        _setup_django_config_files()            
-        _build_and_setup_glass()        
-        
-        # set up the start scripts
-            
-        # run the tests
-
-        # FOR DEBUG ONLY, make site pagackes available, so no need to compile numpy ect
-        if DEBUG:
-            #run('rm %s' % (os.path.join(_E.INSTALLPATH, _S.PYENV_DIR, 'lib', 'python2.7','no-global-site-packages.txt')))
-            pass # we already enabled site packages, because no need to build numpy..
 
 
 
-    
-    with cd(_S.ROOT_PATH):
-        if files.exists('_current'):
-            run('rm _current')
-        run('ln -s %s _current' % _E.VERSION.version_str)
-
-        
-        if not DEBUG:
-            run('rm -rf _current/{TMPDIR}'.format(**_S))
-        else:
-            debugmsg("Skipping cleanup")
-        
-        
-        
-    # create startup scripts
-
-
-
-def _copy_files_server():
-
-    newsubdirs = [
-        _S.APPS.DIR,
-        _S.APPS.MEDIA_DIR,
-        _S.TMPDIR,
-        'deploy',
-    ]
-
-    # dirs to copy (src, dest)
-    dirstocopy = [
-        (_S.SRC.DJANGODIR, _S.APPS.DIR),
-        ('deploy', 'deploy'),
-    ]
-
-    # single files to copy (src, dest, destname)
-    filestocopy = [
-        (_S.SRC.PIP_REQ_RPATH_SRV, _S.TMPDIR, 'pip_requirements.txt')
-    ]
-
-    _setup_dirs_and_copy_files(filestocopy, dirstocopy, newsubdirs)
-
-
-
-
-
-def _generate_django_config_file_str(settings_dic):
-
-# FOR TESTING _generate...
-#    sett = AttrDict({
-#        'DATABASE' : {
-#            'NAME'  : 'blabla',
-#            'CONN'  : {'H':12, 'P':154}
-#        },
-#        'USER' : 'Bla'
-#    })
-    
-    def d_eval(val, prefix=''):
-        #print val, prefix
-
-        if isinstance(val,dict) or isinstance(val, AttrDict):
-            if len(prefix)>0:
-                prefix += '_'
-            s = []
-            for k, v in val.items():
-                s.extend(d_eval(v, prefix+k))
-            return s
-        else:
-            if len(prefix) > 24:
-                assert "error in creating config file.. string too long, adjust script"
-            v = ("'%s'"%str(val)) if type(val)==str else str(val) # only put string in '..'
-            #print prefix, v
-            return ['%-24s = %s' % (prefix, v)]
-    
-    return '\n'.join(sorted(d_eval(settings_dic)))
-
-
-
-
-
-
-def _setup_py_pip_venv(src_piprq_filename):
-    '''setup python, pip and virtualenv
-    
-    assumption:
-    * we have  python, but nothing else (should work with python2 and 3, but will get venv with py2)
-    * will be installed in the current dir
-    solution: create a local python (~~/.local/bin)
-    which is used to create a virtualenv
-    Make sure the local pip is on path "~/.local/bin"
-    
-    http://forcecarrier.wordpress.com/2013/07/26/installing-pip-virutalenv-in-sudo-free-way/
-    '''
-    
-    inform("Setup Python, Pip and the VirtualEnv")
-
-    with settings(warn_only=True):
-        cmd = run('python --version')        
-        if cmd.failed or '2.7' not in cmd:
-            abort('No python 2.7 available remote... aborting')
-        cmd = run('pip -V')
-        if cmd.failed or 'python 2.7' not in cmd:
-            warnn('no pip found remote. getting and installing a remote version into ~/.local/bin')
-            run('curl -O https://bootstrap.pypa.io/get-pip.py')
-            run('python get-pip.py --user')
-            warnn('Make sure the local pip is on path "~/.local/bin" (~/.bashrc)')
-            if not console.confirm('Finished putting it on path?'):
-                abort('then do it now!!')
-
-    with settings(warn_only=True):
-        if run('virtualenv --version').failed:
-            warnn('no virtualenv found, installing a local (--user) one using pip')
-            run('pip install --user virtualenv')
-    
-    run('virtualenv -p python --system-site-packages %s' %_S.PYENV_DIR) #using numpy from system..
-
-
-    # upload pip requirements file
-    tmp_path = join(_E.INSTALLPATH, _S.TMPDIR)
-    piprq_fqfn = join(tmp_path, _S.SRC.PIP_REQ_FILE)
-    piprq_template = join(_S.SRC.TEMPLATES, src_piprq_filename)
-
-    # create/upload config in progdir
-    run('mkdir -p %s' % tmp_path)
-    files.upload_template(piprq_template,
-                          piprq_fqfn)
-#                          context = _DA)
-
-
-    # instal python packages into virtualenv
-    with prefix('source %s' % os.path.join(_S.PYENV_DIR, 'bin/activate')):
-#        if not DEBUG:
-        run('pip install -r {TMPDIR}/{SRC.PIP_REQ_FILE}'.format(**_S))
+# MAJORCLEANUP1 #TODO: remove all of the following
+#
+#def _copy_files_server():
+#
+#    newsubdirs = [
+#        _S.APPS.DIR,
+#        _S.APPS.MEDIA_DIR,
+#        _S.TMPDIR,
+#        'deploy',
+#    ]
+#
+#    # dirs to copy (src, dest)
+#    dirstocopy = [
+#        (_S.SRC.DJANGODIR, _S.APPS.DIR),
+#        ('deploy', 'deploy'),
+#    ]
+#
+#    # single files to copy (src, dest, destname)
+#    filestocopy = [
+#        (_S.SRC.PIP_REQ_RPATH_SRV, _S.TMPDIR, 'pip_requirements.txt')
+#    ]
+#
+#    _setup_dirs_and_copy_files(filestocopy, dirstocopy, newsubdirs)
+#
+#
+#
+#
+#
+#def _generate_django_config_file_str(settings_dic):
+#
+## FOR TESTING _generate...
+##    sett = AttrDict({
+##        'DATABASE' : {
+##            'NAME'  : 'blabla',
+##            'CONN'  : {'H':12, 'P':154}
+##        },
+##        'USER' : 'Bla'
+##    })
+#    
+#    def d_eval(val, prefix=''):
+#        #print val, prefix
+#
+#        if isinstance(val,dict) or isinstance(val, AttrDict):
+#            if len(prefix)>0:
+#                prefix += '_'
+#            s = []
+#            for k, v in val.items():
+#                s.extend(d_eval(v, prefix+k))
+#            return s
 #        else:
-#            debugmsg('skipping local installation of python modules') # because build of numpy take some time..
-
-# make sure to have on opensuse: (sudo zypper)
-# - python-numpy-devel
-# - python-scipy-devel
-# - python-matplotlib-devel
-
-
-
-def _setup_django_config_files(): #TODO remove this function
-    ''' set up the config files
-    Assumptions:
-    * we are currently in the install dir..
-    '''
-
-    inform('Setup Django Configuration Files')
-
-    sett = _S.django_celery_worker_config
-    sett.update({'ROLE': 'production_worker'}) # or use env.django_role
-
-    cstr = _generate_django_config_file_str(sett)
-
-    run('mkdir -p {APPS.SETTINGS_RPATH}'.format(**_S))        #TODO remove this, it should be in the main tree by now
-    run('touch {APPS.SETTINGS_MACHINE}'.format(**_S))
-    files.append(_S.APPS.SETTINGS_MACHINE, cstr) #, escape=False)
-
-    puts(colors.magenta("Getting Secrets from file or console..."))
-    # set up the secrets in the config file
-    required_secrets = [
-        'DATABASE_USER',
-        'DATABASE_PASSWORD',
-        'BROKER_USER',
-        'BROKER_PASSWORD'
-    ]
-    secrets = {}
-    
-    try:
-        import secret_settings
-    except ImportError:
-        secret_settings = None
-        warnn('No predefined secrets found in secret_settings.py!\nPlease enter the secrets manually:')
-    for sec in required_secrets:
-        try:
-            val = secret_settings._[sec]
-        except (KeyError, AttributeError):
-            val = ''
-        if not DEBUG:
-            val = console.prompt('%s: ' % sec, default=val)
-        else:
-            debugmsg('skipping confirmation of secret settings (%s)'%sec)
-        secrets[sec] = val
-        
-    cstr = _generate_django_config_file_str(secrets)
-    files.append(_S.APPS.SETTINGS_SECRETS, cstr)
-
-
-    # create versions file to keep track of version numbers
-    cstr = '# version numbers\n\nversion = '
-    cstr += repr(_E.VERSION)[1:].replace(',', ',\n   ').replace('{', '{\n    ').replace('}', '\n}') # convert to real python code with nice format
-    files.append(_S.APPS.SETTINGS_VERSION, cstr)
-
+#            if len(prefix) > 24:
+#                assert "error in creating config file.. string too long, adjust script"
+#            v = ("'%s'"%str(val)) if type(val)==str else str(val) # only put string in '..'
+#            #print prefix, v
+#            return ['%-24s = %s' % (prefix, v)]
+#    
+#    return '\n'.join(sorted(d_eval(settings_dic)))
+#
+#
+#
+#
+#
+#
+#def _setup_py_pip_venv(src_piprq_filename):
+#    '''setup python, pip and virtualenv
+#    
+#    assumption:
+#    * we have  python, but nothing else (should work with python2 and 3, but will get venv with py2)
+#    * will be installed in the current dir
+#    solution: create a local python (~~/.local/bin)
+#    which is used to create a virtualenv
+#    Make sure the local pip is on path "~/.local/bin"
+#    
+#    http://forcecarrier.wordpress.com/2013/07/26/installing-pip-virutalenv-in-sudo-free-way/
+#    '''
+#    
+#    inform("Setup Python, Pip and the VirtualEnv")
+#
+#    with settings(warn_only=True):
+#        cmd = run('python --version')        
+#        if cmd.failed or '2.7' not in cmd:
+#            abort('No python 2.7 available remote... aborting')
+#        cmd = run('pip -V')
+#        if cmd.failed or 'python 2.7' not in cmd:
+#            warnn('no pip found remote. getting and installing a remote version into ~/.local/bin')
+#            run('curl -O https://bootstrap.pypa.io/get-pip.py')
+#            run('python get-pip.py --user')
+#            warnn('Make sure the local pip is on path "~/.local/bin" (~/.bashrc)')
+#            if not console.confirm('Finished putting it on path?'):
+#                abort('then do it now!!')
+#
+#    with settings(warn_only=True):
+#        if run('virtualenv --version').failed:
+#            warnn('no virtualenv found, installing a local (--user) one using pip')
+#            run('pip install --user virtualenv')
+#    
+#    run('virtualenv -p python --system-site-packages %s' %_S.PYENV_DIR) #using numpy from system..
+#
+#
+#    # upload pip requirements file
+#    tmp_path = join(_E.INSTALLPATH, _S.TMPDIR)
+#    piprq_fqfn = join(tmp_path, _S.SRC.PIP_REQ_FILE)
+#    piprq_template = join(_S.SRC.TEMPLATES, src_piprq_filename)
+#
+#    # create/upload config in progdir
+#    run('mkdir -p %s' % tmp_path)
+#    files.upload_template(piprq_template,
+#                          piprq_fqfn)
+##                          context = _DA)
+#
+#
+#    # instal python packages into virtualenv
+#    with prefix('source %s' % os.path.join(_S.PYENV_DIR, 'bin/activate')):
+##        if not DEBUG:
+#        run('pip install -r {TMPDIR}/{SRC.PIP_REQ_FILE}'.format(**_S))
+##        else:
+##            debugmsg('skipping local installation of python modules') # because build of numpy take some time..
+#
+## make sure to have on opensuse: (sudo zypper)
+## - python-numpy-devel
+## - python-scipy-devel
+## - python-matplotlib-devel
+#
+#
+#
+#def _setup_django_config_files(): #TODO remove this function
+#    ''' set up the config files
+#    Assumptions:
+#    * we are currently in the install dir..
+#    '''
+#
+#    inform('Setup Django Configuration Files')
+#
+#    sett = _S.django_celery_worker_config
+#    sett.update({'ROLE': 'production_worker'}) # or use env.django_role
+#
+#    cstr = _generate_django_config_file_str(sett)
+#
+#    run('mkdir -p {APPS.SETTINGS_RPATH}'.format(**_S))        #TODO remove this, it should be in the main tree by now
+#    run('touch {APPS.SETTINGS_MACHINE}'.format(**_S))
+#    files.append(_S.APPS.SETTINGS_MACHINE, cstr) #, escape=False)
+#
+#    puts(colors.magenta("Getting Secrets from file or console..."))
+#    # set up the secrets in the config file
+#    required_secrets = [
+#        'DATABASE_USER',
+#        'DATABASE_PASSWORD',
+#        'BROKER_USER',
+#        'BROKER_PASSWORD'
+#    ]
+#    secrets = {}
+#    
+#    try:
+#        import secret_settings
+#    except ImportError:
+#        secret_settings = None
+#        warnn('No predefined secrets found in secret_settings.py!\nPlease enter the secrets manually:')
+#    for sec in required_secrets:
+#        try:
+#            val = secret_settings._[sec]
+#        except (KeyError, AttributeError):
+#            val = ''
+#        if not DEBUG:
+#            val = console.prompt('%s: ' % sec, default=val)
+#        else:
+#            debugmsg('skipping confirmation of secret settings (%s)'%sec)
+#        secrets[sec] = val
+#        
+#    cstr = _generate_django_config_file_str(secrets)
+#    files.append(_S.APPS.SETTINGS_SECRETS, cstr)
+#
+#
+#    # create versions file to keep track of version numbers
+#    cstr = '# version numbers\n\nversion = '
+#    cstr += repr(_E.VERSION)[1:].replace(',', ',\n   ').replace('{', '{\n    ').replace('}', '\n}') # convert to real python code with nice format
+#    files.append(_S.APPS.SETTINGS_VERSION, cstr)
+#
 
   
 def _generate_version_information():
@@ -551,78 +513,27 @@ def _build_and_setup_glass(doBuild = True):
 
 
 
-
-def _setup_dirs_and_copy_files_worker():
-    '''(insert functionname)
-
-    assumes:
-    * existing _E.VERSION version strings   
-    
-    #TODO: replace this with the more general version...
-    '''
-    
-    inform('Setup the Dirs and copy the files for worker config')
-    assert(_E.VERSION.version_str)
-
-
-    _E.INSTALLPATH = os.path.join(_S.ROOT_PATH, _E.VERSION.version_str) #'real' install dir
-
-    check_or_create_dirs([
-        _S.ROOT_PATH,
-        _S.BIN_DIR,
-        _E.INSTALLPATH,
-    ])
-
-    # dirs to create    
-    dirlist = [
-        _S.APPS.DIR,
-        _S.APPS.MEDIA_DIR,
-        _S.TMPDIR,
-    ]
-
-    # dirs to copy (src, dest)
-    dirsss = [
-        (_S.SRC.DJANGODIR, _S.APPS.DIR)  
-    ]
-
-    # single files to copy (src, dest)
-    filesss = [
-        (_S.SRC.PIP_REQ_RPATH_WRK, os.path.join(_S.TMPDIR, _S.SRC.PIP_REQ_FILE))    
-    ]
-
-
-    with cd(_E.INSTALLPATH):
-        
-        full_dirlist = [os.path.join(_E.INSTALLPATH, d) for d in dirlist]
-        check_or_create_dirs(full_dirlist)
-        
-        for srcdir, destdir in dirsss:
-            project.rsync_project(
-                remote_dir=os.path.join(_E.INSTALLPATH, destdir),
-                local_dir=os.path.join(srcdir, '') #appends trialing slash
-            )
-            
-        for srcfile, destdir in filesss:
-            put(local_path=srcfile, remote_path=os.path.join(_E.INSTALLPATH, destdir))
-
-
-
-
-
-
-def _setup_dirs_and_copy_files(filestocopy=None, dirstocopy=None, newsubdirs=None):
-    '''copies a list of files and dirs; and creates a few empty folders'''    
-    
-    inform('Setup the dirs and copy the files for the server')
-    assert(_E.INSTALLPATH)
-    
-    run('mkdir -p %s' % _E.INSTALLPATH)
-    
+# MAJORCLEANUP1 #TODO: remove all of the following
+#
+#def _setup_dirs_and_copy_files_worker():
+#    '''(insert functionname)
+#
+#    assumes:
+#    * existing _E.VERSION version strings   
+#    
+#    #TODO: replace this with the more general version...
+#    '''
+#    
+#    inform('Setup the Dirs and copy the files for worker config')
+#    assert(_E.VERSION.version_str)
+#
+#
+#    _E.INSTALLPATH = os.path.join(_S.ROOT_PATH, _E.VERSION.version_str) #'real' install dir
+#
 #    check_or_create_dirs([
 #        _S.ROOT_PATH,
 #        _S.BIN_DIR,
 #        _E.INSTALLPATH,
-#        'deploy',
 #    ])
 #
 #    # dirs to create    
@@ -630,46 +541,98 @@ def _setup_dirs_and_copy_files(filestocopy=None, dirstocopy=None, newsubdirs=Non
 #        _S.APPS.DIR,
 #        _S.APPS.MEDIA_DIR,
 #        _S.TMPDIR,
-#        'deploy',
 #    ]
 #
 #    # dirs to copy (src, dest)
 #    dirsss = [
-#        (_S.SRC.DJANGODIR, _S.APPS.DIR),
-#        ('deploy', 'deploy'),
+#        (_S.SRC.DJANGODIR, _S.APPS.DIR)  
 #    ]
 #
 #    # single files to copy (src, dest)
 #    filesss = [
-#        (_S.SRC.PIP_REQ_RPATH_SRV, os.path.join(_S.TMPDIR, _S.SRC.PIP_REQ_FILE_SRV))    
+#        (_S.SRC.PIP_REQ_RPATH_WRK, os.path.join(_S.TMPDIR, _S.SRC.PIP_REQ_FILE))    
 #    ]
-
-
-    with cd(_E.INSTALLPATH):
-        
-        full_dirlist = [os.path.join(_E.INSTALLPATH, d) for d in newsubdirs]
-        check_or_create_dirs(full_dirlist)
-        
-        for srcdir, destdir in dirstocopy:
-            fulldestdir = os.path.join(_E.INSTALLPATH, destdir)
-            run('mkdir -p %s' % fulldestdir)
-            project.rsync_project(
-                remote_dir=fulldestdir,
-                local_dir=os.path.join(srcdir, '') #appends trialing slash
-            )
-            
-
-        for srcfile, destdir, destfname in filestocopy:
-            fulldestdir = os.path.join(_E.INSTALLPATH, destdir)
-            run('mkdir -p %s' % fulldestdir)
-            if destfname is not None:
-                fdestpath = os.path.join(fulldestdir, destfname)
-            else:
-                fdestpath = fulldestdir
-            put(local_path=srcfile, remote_path=fdestpath)
-            
-            
-#        run('touch %s'%os.path.join(_S.TMPDIR, _S.SRC.PIP_REQ_FILE))
+#
+#
+#    with cd(_E.INSTALLPATH):
+#        
+#        full_dirlist = [os.path.join(_E.INSTALLPATH, d) for d in dirlist]
+#        check_or_create_dirs(full_dirlist)
+#        
+#        for srcdir, destdir in dirsss:
+#            project.rsync_project(
+#                remote_dir=os.path.join(_E.INSTALLPATH, destdir),
+#                local_dir=os.path.join(srcdir, '') #appends trialing slash
+#            )
+#            
+#        for srcfile, destdir in filesss:
+#            put(local_path=srcfile, remote_path=os.path.join(_E.INSTALLPATH, destdir))
+#
+#
+#
+#
+#
+#
+#def _setup_dirs_and_copy_files(filestocopy=None, dirstocopy=None, newsubdirs=None):
+#    '''copies a list of files and dirs; and creates a few empty folders'''    
+#    
+#    inform('Setup the dirs and copy the files for the server')
+#    assert(_E.INSTALLPATH)
+#    
+#    run('mkdir -p %s' % _E.INSTALLPATH)
+#    
+##    check_or_create_dirs([
+##        _S.ROOT_PATH,
+##        _S.BIN_DIR,
+##        _E.INSTALLPATH,
+##        'deploy',
+##    ])
+##
+##    # dirs to create    
+##    dirlist = [
+##        _S.APPS.DIR,
+##        _S.APPS.MEDIA_DIR,
+##        _S.TMPDIR,
+##        'deploy',
+##    ]
+##
+##    # dirs to copy (src, dest)
+##    dirsss = [
+##        (_S.SRC.DJANGODIR, _S.APPS.DIR),
+##        ('deploy', 'deploy'),
+##    ]
+##
+##    # single files to copy (src, dest)
+##    filesss = [
+##        (_S.SRC.PIP_REQ_RPATH_SRV, os.path.join(_S.TMPDIR, _S.SRC.PIP_REQ_FILE_SRV))    
+##    ]
+#
+#
+#    with cd(_E.INSTALLPATH):
+#        
+#        full_dirlist = [os.path.join(_E.INSTALLPATH, d) for d in newsubdirs]
+#        check_or_create_dirs(full_dirlist)
+#        
+#        for srcdir, destdir in dirstocopy:
+#            fulldestdir = os.path.join(_E.INSTALLPATH, destdir)
+#            run('mkdir -p %s' % fulldestdir)
+#            project.rsync_project(
+#                remote_dir=fulldestdir,
+#                local_dir=os.path.join(srcdir, '') #appends trialing slash
+#            )
+#            
+#
+#        for srcfile, destdir, destfname in filestocopy:
+#            fulldestdir = os.path.join(_E.INSTALLPATH, destdir)
+#            run('mkdir -p %s' % fulldestdir)
+#            if destfname is not None:
+#                fdestpath = os.path.join(fulldestdir, destfname)
+#            else:
+#                fdestpath = fulldestdir
+#            put(local_path=srcfile, remote_path=fdestpath)
+#            
+#            
+##        run('touch %s'%os.path.join(_S.TMPDIR, _S.SRC.PIP_REQ_FILE))
 
 
 
